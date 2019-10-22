685c0a09bec074b51813574494f735a2a644d198
Emery Tate <tateejt@gmail.com>
2019-10-22 04:37:30 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,6 +7,6 @@ class Anagram
   end
   
   def match(array)
-    array.select do |element| (@word.split("").sort) == 
+    array.select do |element| (@word.split("").sort) == (element(".split"))
   end
 end
